11c6a688a7244bbe03908192b71bbeb7c20ccffa
Daniel Alva <luisdaniel9986@gmail.com>
2026-01-22 18:50:37 -0500

    fix

--- database/migrations/tenant/2023_10_06_160944_create_categories_table.php
+++ database/migrations/tenant/2023_10_06_160944_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->enum('status',['ACTIVE','INACTIVE'])->default('ACTIVE');
+            $table->boolean('is_default')->default(false);
             $table->timestamps();
         });
     }
