416ee21a742555b3b0393818e493502d6403807f
Daniel Alva <luisdaniel9986@gmail.com>
2026-01-21 19:39:08 -0500

    fix

--- routes/tenant/waiter_counter/web.php
+++ routes/tenant/waiter_counter/web.php
@@ -11,7 +11,6 @@ Route::group(["prefix" => "mostrador_mesero"], function () {
         Route::get('index', [WCounterController::class, 'index'])->name('tenant.mostrador_mesero.mostrador.index');
         Route::get('create/{table}', [WCounterController::class, 'create'])->name('tenant.mostrador_mesero.mostrador.create');
         Route::get('getAll', [WCounterController::class, 'getAll'])->name('tenant.mostrador_mesero.mostrador.getAll');
-        Route::get('getCustomerAccount/{id}', [WCounterController::class, 'getCustomerAccount'])->name('tenant.cuentas.cliente.getCustomerAccount');
         Route::post('store', [WCounterController::class, 'store'])->name('tenant.mostrador_mesero.mostrador.store');
         Route::get('get-order-table/{table}', [WCounterController::class, 'getOrderTable'])->name('tenant.mostrador_mesero.mostrador.getOrderTable');
         Route::put('update/{id}', [WCounterController::class, 'update'])->name('tenant.mostrador_mesero.mostrador.update');
