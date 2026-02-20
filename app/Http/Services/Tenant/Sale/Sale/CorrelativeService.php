bdca7b0e9b766db15f0c04d5d60fd68cb14711ab
Daniel Alva <luisdaniel9986@gmail.com>
2026-02-20 17:40:20 -0500

    fix serialization

--- app/Http/Services/Tenant/Sale/Sale/CorrelativeService.php
+++ app/Http/Services/Tenant/Sale/Sale/CorrelativeService.php
@@ -14,34 +14,28 @@ class CorrelativeService
 
     public function __construct() {}
 
-/*
+    /*
 {#2181 // app\Http\Services\Tenant\Sale\Sale\SaleService.php:41
   +"correlative": "1"
   +"serie": "NV01"
 }
 */
-    public static function getCorrelative($type_sale_id):object
+    public static function getCorrelative($type_sale_id): object
     {
         $correlative        =   null;
-        $serie              =   null;
 
         //======= CONTABILIZANDO SI HAY DOCUMENTOS DE VENTA EMITIDOS PARA EL TYPE SALE ======
-        $count_sales        =   Sale::where('type_sale_id',$type_sale_id)->count();
+        $last_correlative   =   Sale::where('type_sale_id', $type_sale_id)->max('correlative');
+        $serialization      =   DocumentSerialization::where('company_id', 1)->where('document_type_id', $type_sale_id)->first();
 
-        $serialization      =   DocumentSerialization::where('company_id',1)->where('document_type_id',$type_sale_id)->first();
-
-        //==== SI LA CANT ES 0 =====
-        if ($count_sales === 0) {
-
-            //====== INICIAR DESDE EL STARTING NUMBER =======
-            $correlative        =   $serialization->start_number;
-            $serie              =   $serialization->serie;
-        } else {
-            //======= EN CASO YA EXISTAN DOCUMENTOS DE VENTA DEL TYPE SALE ======
-            $correlative        =   $count_sales->cant  +   1;
-            $serie              =   $serialization->serie;
+        if (!$serialization) {
+            throw new Exception('No existe serialización configurada');
         }
 
-        return (object)['correlative' => $correlative, 'serie' => $serie];
+        $correlative = $last_correlative
+            ? $last_correlative + 1
+            : $serialization->start_number;
+
+        return (object)['correlative' => $correlative, 'serie' => $serialization->serie];
     }
 }
