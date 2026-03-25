e10e852736d4c42bbd67083ca1469c59aae95289
Daniel Alva <luisdaniel9986@gmail.com>
2026-03-25 09:48:31 -0500

    fix

--- resources/views/reports/report_contable/pdf/pdf.blade.php
+++ resources/views/reports/report_contable/pdf/pdf.blade.php
@@ -118,7 +118,7 @@
 
                 <!-- Columna 2: Información de la empresa -->
                 <td style="width: 60%; text-align: left;">
-                    <h2 style="margin: 0; font-size: 14px; color: #3a6ea5;">{{ $company->abbreviated_business_name }}</h2>
+                    <h2 style="margin: 0; font-size: 14px; color: #3a6ea5;">{{ $company->abbreviated_business_name  }}</h2>
                     <p style="margin: 0; font-size: 14px; color: #555;">RUC: {{ $company->ruc }}</p>
                     <p style="margin: 0; font-size: 14px; color: #555;">{{ $company->fiscal_address }}</p>
                     <p style="margin: 0; font-size: 14px; color: #555;">Teléfono: {{ $company->phone }}</p>
