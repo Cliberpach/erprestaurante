9db5f6ef0c8103eafc7327c646cc76758baffaeb
Daniel Alva <luisdaniel9986@gmail.com>
2026-05-12 10:15:22 -0500

    fix job

--- app/Jobs/Landlord/Company/CompanyStoreJob.php
+++ app/Jobs/Landlord/Company/CompanyStoreJob.php
@@ -10,13 +10,13 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use App\Services\Landlord\Company\CompanyServiceManager;
 use Illuminate\Support\Facades\Log;
 
 class CompanyStoreJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $timeout = 300;
     public array $data;
 
     /**
