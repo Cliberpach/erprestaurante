ac96ffa0dba488267f8045db1597ad78b84d1caa
Daniel Alva <luisdaniel9986@gmail.com>
2026-05-12 18:54:09 -0500

    test time migrations tenant

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 use Spatie\Multitenancy\Models\Tenant as BaseTenant;
 
 class Tenant extends BaseTenant
@@ -18,18 +19,18 @@ class Tenant extends BaseTenant
 
     public static function booted()
     {
-        static::creating(fn (Tenant $tenant) => $tenant->createDatabase($tenant));
-        static::created(fn (Tenant $tenant) => $tenant->runMigrationsSeeders($tenant));
+        static::creating(fn(Tenant $tenant) => $tenant->createDatabase($tenant));
+        static::created(fn(Tenant $tenant) => $tenant->runMigrationsSeeders($tenant));
     }
 
     public function createDatabase($tenant)
     {
-        $database_name = "tenancy_".$tenant->domain;
-        $database = Str::of($database_name)->replace('.','_')->lower()->__toString();
+        $database_name = "tenancy_" . $tenant->domain;
+        $database = Str::of($database_name)->replace('.', '_')->lower()->__toString();
 
         $query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?";
-        $db = DB::select($query,[$database]);
-        if(empty($db)){
+        $db = DB::select($query, [$database]);
+        if (empty($db)) {
             DB::connection('tenant')->statement("CREATE DATABASE {$database};");
             $tenant->database = $database;
         }
@@ -38,9 +39,16 @@ class Tenant extends BaseTenant
     public function runMigrationsSeeders($tenant)
     {
         $tenant->refresh();
-        Artisan::call('tenants:artisan',[
-            'artisanCommand'=>'migrate --path=database/migrations/tenant --database=tenant --seed --force',
-            '--tenant'=> "{$tenant->id}"
+        $t = microtime(true);
+        Log::channel('tenant_store')->info('MIGRACIONES INICIANDO');
+
+        Artisan::call('tenants:artisan', [
+            'artisanCommand' => 'migrate --path=database/migrations/tenant --database=tenant --seed --force',
+            '--tenant' => "{$tenant->id}"
+        ]);
+
+        Log::channel('tenant_store')->info('MIGRACIONES COMPLETADAS', [
+            'seconds' => round(microtime(true) - $t, 2)
         ]);
     }
 }
