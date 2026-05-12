aad75348554d3c889c03e24e0909ad010d90a943
Daniel Alva <luisdaniel9986@gmail.com>
2026-05-12 15:48:48 -0500

    fix not included testseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -74,7 +74,7 @@ class DatabaseSeeder extends Seeder
         $this->call(SupplierSeeder::class);
         $this->call(ProofPaymentSeeder::class);
         $this->call(TypeFieldSeeder::class);
-        $this->call(TestSeeder::class);
+        //$this->call(TestSeeder::class);
         $this->call(PaymentConditionSeeder::class);
         $this->call(CostCenterSeeder::class);
         //$this->call(TenantUserSeeder::class);
